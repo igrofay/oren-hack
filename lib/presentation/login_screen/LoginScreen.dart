9a3ab15387fcf234f36b332e5cce27f15eb7f7b4
araik.gukasyan <araik.gukasyan@avalab.io>
2022-12-03 06:53:29 +0500

    okok

--- lib/presentation/login_screen/LoginScreen.dart
+++ lib/presentation/login_screen/LoginScreen.dart
@@ -14,110 +14,116 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BackgroundScaffold(
-        back: NeMainBackgroundImage,
-        child: SizedBox(
-          child: Flex(
-            direction: Axis.horizontal,
-            children: [
-              Flexible(
-                  flex: 1,
-                  child: SizedBox(
-                      width: double.infinity,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SvgPicture.asset(RoundedSunPrimary),
-                          kDefaultVerticalPadding,
-                          Text(
-                            "Счастливый билет",
-                            style: defaultTextStyle(
-                                size: 26,
-                                fontWeight: FontWeight.w500,
-                                color: primary),
-                          )
-                        ],
-                      ))),
-              Flexible(
-                  child: SizedBox(
-                      width: double.infinity,
-                      child: LayoutBuilder(builder: (_, constraints) {
-                        return Center(
-                          child: SizedBox(
-                            width: constraints.maxWidth / 2,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Войти в аккаунт",
-                                  style: defaultTextStyle(
-                                      size: 32, fontWeight: FontWeight.bold),
-                                ),
-                                CupertinoButton(
-                                  onPressed: () {},
-                                  padding: EdgeInsets.zero,
-                                  minSize: 0,
-                                  child: Text(
-                                    "Или зарегистрируйте аккаунт!",
-                                    style: defaultTextStyle(
-                                        size: 18,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white70),
-                                  ),
-                                ),
-                                kDefaultVerticalPadding,
-                                CustomTextField(hintText: "E-mail"),
-                                kDefaultVerticalPadding,
-                                CustomTextField(hintText: "Пароль"),
-                                kDefaultVerticalPadding,
-                                CustomButton(
-                                  buttonColor: Colors.white,
-                                  textColor: primary,
-                                  text: "Зарегистрироваться",
-                                  onPressed: () {},
-                                ),
-                                kDefaultVerticalPadding,
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      height: 25,
-                                      width: 25,
-                                      decoration: BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Colors.transparent,
-                                          border: Border.all(
-                                              color: Colors.white70)),
-                                      child: Checkbox(
-                                          fillColor: MaterialStateProperty.all(
-                                              Colors.transparent),
-                                          value: true,
-                                          onChanged: (value) {}),
-                                    ),
-                                    SizedBox(
-                                      width: kDefaultHorizontalPaddingValue / 2,
-                                    ),
-                                    Text(
-                                      "я организация",
-                                      style: defaultTextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.white70)
-                                          .copyWith(
-                                              decoration:
-                                                  TextDecoration.underline),
-                                    )
-                                  ],
-                                )
-                              ],
-                            ),
-                          ),
-                        );
-                      })))
-            ],
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(NeMainBackgroundImage),
+            fit: BoxFit.cover,
           ),
-        ));
+        ),
+        child: Flex(
+          direction: Axis.horizontal,
+          children: [
+            Flexible(
+                flex: 1,
+                child: SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(RoundedSunPrimary),
+                        kDefaultVerticalPadding,
+                        Text(
+                          "Счастливый билет",
+                          style: defaultTextStyle(
+                              size: 26,
+                              fontWeight: FontWeight.w500,
+                              color: primary),
+                        )
+                      ],
+                    ))),
+            Flexible(
+                child: SizedBox(
+                    width: double.infinity,
+                    child: LayoutBuilder(builder: (_, constraints) {
+                      return Center(
+                        child: SizedBox(
+                          width: constraints.maxWidth / 2,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Войти в аккаунт",
+                                style: defaultTextStyle(
+                                    size: 32, fontWeight: FontWeight.bold),
+                              ),
+                              CupertinoButton(
+                                onPressed: () {},
+                                padding: EdgeInsets.zero,
+                                minSize: 0,
+                                child: Text(
+                                  "Или зарегистрируйте аккаунт!",
+                                  style: defaultTextStyle(
+                                      size: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white70),
+                                ),
+                              ),
+                              kDefaultVerticalPadding,
+                              CustomTextField(hintText: "E-mail"),
+                              kDefaultVerticalPadding,
+                              CustomTextField(hintText: "Пароль"),
+                              kDefaultVerticalPadding,
+                              CustomButton(
+                                buttonColor: Colors.white,
+                                textColor: primary,
+                                text: "Зарегистрироваться",
+                                onPressed: () {},
+                              ),
+                              kDefaultVerticalPadding,
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 25,
+                                    width: 25,
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.transparent,
+                                        border:
+                                            Border.all(color: Colors.white70)),
+                                    child: Checkbox(
+                                        fillColor: MaterialStateProperty.all(
+                                            Colors.transparent),
+                                        value: true,
+                                        onChanged: (value) {}),
+                                  ),
+                                  SizedBox(
+                                    width: kDefaultHorizontalPaddingValue / 2,
+                                  ),
+                                  Text(
+                                    "я организация",
+                                    style: defaultTextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white70)
+                                        .copyWith(
+                                            decoration:
+                                                TextDecoration.underline),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    })))
+          ],
+        ),
+      ),
+    );
   }
 }
