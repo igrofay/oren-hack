35e6c93861f7964318e9bd299bcc6394bba2b50b
Igor Starkov <igor.starkov@avalab.io>
2022-12-03 06:33:37 +0500

    изменение датаклассов токена

--- lib/data/models/token_set.dart
+++ lib/data/models/token_set.dart
@@ -5,16 +5,12 @@ part 'token_set.g.dart';
 @JsonSerializable()
 class TokenSet {
 
-  String grandType;
   String refreshToken;
   String accessToken;
   String userType;
-  String username;
-  String password;
 
 
-  TokenSet(this.grandType, this.refreshToken, this.accessToken, this.userType,
-      this.username, this.password);
+  TokenSet(this.refreshToken, this.accessToken, this.userType);
 
   factory TokenSet.fromJson(Map<String, dynamic> json) =>
       _$TokenSetFromJson(json);
--- lib/data/models/token_set.g.dart
+++ lib/data/models/token_set.g.dart
@@ -7,19 +7,13 @@ part of 'token_set.dart';
 // **************************************************************************
 
 TokenSet _$TokenSetFromJson(Map<String, dynamic> json) => TokenSet(
-      json['grandType'] as String,
       json['refreshToken'] as String,
       json['accessToken'] as String,
       json['userType'] as String,
-      json['username'] as String,
-      json['password'] as String,
     );
 
 Map<String, dynamic> _$TokenSetToJson(TokenSet instance) => <String, dynamic>{
-      'grandType': instance.grandType,
       'refreshToken': instance.refreshToken,
       'accessToken': instance.accessToken,
       'userType': instance.userType,
-      'username': instance.username,
-      'password': instance.password,
     };
